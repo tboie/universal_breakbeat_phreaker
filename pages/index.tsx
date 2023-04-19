583bbc2b9c438ad1eba3d4d914ac65491637c9ff
tboie <timboie@gmail.com>
2023-04-19 13:30:48 -0400

    layer 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,15 +17,18 @@ let init = false;
 
 let ws0: any;
 let ws1: any;
+let ws2: any;
 
 let regionLoop: any;
 let regionRand: any;
 let regionLayer1: any;
+let regionLayer2: any;
 
 let touchMoved = false;
 
 let players0: Tone.Player[] = [];
 let players1: Tone.Player[] = [];
+let players2: Tone.Player[] = [];
 
 let part: Tone.Part;
 
@@ -109,6 +112,14 @@ export default function Home(props: { folders: string[] }) {
         scrollParent: false,
       });
 
+      ws2 = WaveSurfer.create({
+        container: "#waveform2",
+        height: 200,
+        waveColor: "teal",
+        fillParent: false,
+        scrollParent: false,
+      });
+
       const configZoom = () => {
         const zoomEle = document.querySelector("#zoom") as HTMLInputElement;
         if (zoomEle) {
@@ -118,6 +129,7 @@ export default function Home(props: { folders: string[] }) {
 
           ws0.zoom(minZoom);
           ws1.zoom(minZoom);
+          ws2.zoom(minZoom);
           zoomEle.min = minZoom.toString();
           zoomEle.max = maxZoom.toString();
           zoomEle.value = minZoom.toString();
@@ -147,6 +159,7 @@ export default function Home(props: { folders: string[] }) {
         configZoom();
         ws0.drawer.fireEvent("redraw");
         ws1.drawer.fireEvent("redraw");
+        ws2.drawer.fireEvent("redraw");
       });
 
       document.body.addEventListener("touchmove", (event) => {
@@ -230,6 +243,16 @@ export default function Home(props: { folders: string[] }) {
           regionLayer1 = Object.values(ws0.regions.list)[2];
         }
 
+        if (!regionLayer2) {
+          ws0.addRegion({
+            id: "layer2",
+            start: 0,
+            end: seq[seq.length - 1].time + seq[seq.length - 1].duration,
+            loop: false,
+          });
+          regionLayer2 = Object.values(ws0.regions.list)[3];
+        }
+
         ws0.clearMarkers();
         seq.forEach((s) => {
           ws0.addMarker({ time: s.time });
@@ -260,6 +283,10 @@ export default function Home(props: { folders: string[] }) {
     ws1.zoom(0);
     ws1.empty();
     ws1.backend.buffer = undefined;
+
+    ws2.zoom(0);
+    ws2.empty();
+    ws2.backend.buffer = undefined;
   };
 
   const listClick = async (
@@ -276,6 +303,7 @@ export default function Home(props: { folders: string[] }) {
     setFader(0);
     setSelectedFile(folder);
     setLoading(true);
+    setSelectedLayer(0);
 
     let times: any[] = [];
     await fetch(`/drums/${folder}/times.txt`)
@@ -306,11 +334,14 @@ export default function Home(props: { folders: string[] }) {
 
     temp.sort((a, b) => a.i - b.i);
 
+    players0.forEach((p: any) => p.dispose());
+    players0 = [];
+
     players1.forEach((p: any) => p.dispose());
     players1 = [];
 
-    players0.forEach((p: any) => p.dispose());
-    players0 = [];
+    players2.forEach((p: any) => p.dispose());
+    players2 = [];
 
     seq = [];
 
@@ -345,6 +376,12 @@ export default function Home(props: { folders: string[] }) {
             Tone.Time(time).toSeconds() + Tone.Time(value.duration).toSeconds()
           );
           */
+        }
+        if (
+          piece.time >= regionLayer2?.start &&
+          piece.time < regionLayer2?.end
+        ) {
+          players2[value.idx]?.start(time);
         }
       }
 
@@ -419,6 +456,8 @@ export default function Home(props: { folders: string[] }) {
 
     drawLayer(0);
     drawLayer(1, selectedLayer === 1 ? false : true);
+    drawLayer(2, selectedLayer === 2 ? false : true);
+
     //concatBuffers();
   };
 
@@ -583,9 +622,14 @@ export default function Home(props: { folders: string[] }) {
   }, []);
   */
 
-  const findMatches = async () => {
-    players1.forEach((p: any) => p.dispose());
-    players1 = [];
+  const findMatches = async (layer: number) => {
+    if (layer === 1) {
+      players1.forEach((p: any) => p.dispose());
+      players1 = [];
+    } else if (layer === 2) {
+      players2.forEach((p: any) => p.dispose());
+      players2 = [];
+    }
 
     let srcTable: any[] = [];
     srcTable = table.filter((r) => r.n === selectedFile);
@@ -632,9 +676,14 @@ export default function Home(props: { folders: string[] }) {
     );
 
     t_players.sort((a, b) => a.i - b.i);
-    players1 = t_players.map((r) => r.o);
 
-    drawLayer(1);
+    if (layer === 1) {
+      players1 = t_players.map((r) => r.o);
+    } else if (layer === 2) {
+      players2 = t_players.map((r) => r.o);
+    }
+
+    drawLayer(layer);
   };
 
   const drawLayer = (layer: number, selection?: boolean) => {
@@ -651,19 +700,29 @@ export default function Home(props: { folders: string[] }) {
         c_players = players1.map((p: any) =>
           new Tone.Player(p.buffer).toDestination()
         );
+      } else if (layer === 2) {
+        c_players = players2.map((p: any) =>
+          new Tone.Player(p.buffer).toDestination()
+        );
       }
 
       new Tone.Part((time, value) => {
         if (selection) {
-          if (layer === 1) {
-            const piece = seq.find((s) => s.idx === value.idx);
-            if (piece) {
-              if (
-                piece.time >= regionLayer1?.start &&
-                piece.time < regionLayer1?.end
-              ) {
-                c_players[value.idx]?.start(time);
-              }
+          const piece = seq.find((s) => s.idx === value.idx);
+          if (piece) {
+            if (
+              layer === 1 &&
+              piece.time >= regionLayer1?.start &&
+              piece.time < regionLayer1?.end
+            ) {
+              c_players[value.idx]?.start(time);
+            }
+            if (
+              layer === 2 &&
+              piece.time >= regionLayer2?.start &&
+              piece.time < regionLayer2?.end
+            ) {
+              c_players[value.idx]?.start(time);
             }
           }
         } else {
@@ -677,19 +736,25 @@ export default function Home(props: { folders: string[] }) {
         ws0.loadDecodedBuffer(buffer.get());
       } else if (layer === 1) {
         ws1.loadDecodedBuffer(buffer.get());
+      } else if (layer === 2) {
+        ws2.loadDecodedBuffer(buffer.get());
       }
     });
   };
 
-  const layerClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const layerClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    layer: number
+  ) => {
     e.stopPropagation();
     e.preventDefault();
 
-    if (selectedLayer === 0) {
-      drawLayer(1);
-      setSelectedLayer(1);
+    if (layer !== selectedLayer) {
+      drawLayer(layer);
+      setSelectedLayer(layer);
     } else {
       drawLayer(1, true);
+      drawLayer(2, true);
       setSelectedLayer(0);
     }
   };
@@ -708,11 +773,9 @@ export default function Home(props: { folders: string[] }) {
       <main className={styles.main}>
         <h1 className={styles.title}>Universal BreakBeat Phreaker</h1>
 
-        <div
-          id="waveform0"
-          className={`${selectedLayer === 0 ? "layer0" : "layer1"}`}
-        />
+        <div id="waveform0" className={`layer${selectedLayer}`} />
         <div id="waveform1" />
+        <div id="waveform2" />
 
         <div className={styles.controls}>
           <button
@@ -741,7 +804,6 @@ export default function Home(props: { folders: string[] }) {
             {">"}
           </button>
         </div>
-
         <input
           id="scroll"
           type="range"
@@ -775,7 +837,6 @@ export default function Home(props: { folders: string[] }) {
             zoom === Math.floor(window.innerWidth / ws0?.getDuration())
           }
         />
-
         <input
           id="zoom"
           type="range"
@@ -797,7 +858,6 @@ export default function Home(props: { folders: string[] }) {
               )
           }
         />
-
         <input
           id="speed"
           type="range"
@@ -811,7 +871,6 @@ export default function Home(props: { folders: string[] }) {
           }}
           disabled={loading}
         />
-
         <input
           id="fader"
           type="range"
@@ -825,7 +884,6 @@ export default function Home(props: { folders: string[] }) {
           }}
           disabled={loading}
         />
-
         <ul className={styles.playlist}>
           {props.folders.map((folder) => {
             return (
@@ -839,32 +897,34 @@ export default function Home(props: { folders: string[] }) {
             );
           })}
         </ul>
-
         <div className={styles.toolbar}>
           <button
             className={selectedLayer === 1 ? styles.selected : ""}
-            onClick={(e) => layerClick(e)}
+            onClick={(e) => layerClick(e, 1)}
             disabled={loading}
           >
-            Layer
+            1
+          </button>
+
+          <button
+            className={selectedLayer === 2 ? styles.selected : ""}
+            onClick={(e) => layerClick(e, 2)}
+            disabled={loading}
+          >
+            2
           </button>
 
           <button
             disabled={loading || selectedLayer === 0}
-            onClick={() => findMatches()}
+            onClick={() => findMatches(selectedLayer)}
           >
             Match
           </button>
 
           <button onClick={() => {}} disabled={loading}>
-            Break/All
-          </button>
-
-          <button id="download" onClick={() => {}} disabled={loading}>
-            Btn4
+            button
           </button>
         </div>
-
         <div className={styles.toolbar}>
           <button onClick={(e) => originalClick(e)} disabled={loading}>
             Original
